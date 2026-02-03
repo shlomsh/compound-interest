f4b11870a5e690fcc61590bf93ef0eadc0402ba2
shlomsh <shlomsh@gmail.com>
2026-02-03 11:51:24 +0200

    fix deploy path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
 
   // Uncomment if deploying to https://shlomsh.github.io/compound-interest-site/
-  // basePath: '/compound-interest-site',
+  basePath: '/compound-interest-site',
 
   // GitHub Pages doesn't support Next.js Image Optimization API
   images: {
